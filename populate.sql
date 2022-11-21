3838ef73fb518f52fa06aeea7ef4169c28ec8b01
Michael Graves <30810535+mgraves00@users.noreply.github.com>
2022-11-21 07:51:01 -0600

    update db population

--- populate.sql
+++ populate.sql
@@ -1,5 +1,4 @@
 DELETE FROM auth;
 DELETE FROM cookie;
 DELETE FROM miniurl;
-INSERT INTO auth (username,password) VALUES ('bob','letemin');
-INSERT INTO miniurl (hash,url,count) VALUES ('aaa','https://google.com',0);
+INSERT INTO miniurl (hash,url,count) VALUES ('aaa','https://google.com/',0);
